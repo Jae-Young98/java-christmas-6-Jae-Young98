9ad9b4d6a5698df255914362f1f18507a110a357
Jae-Young98 <ygy0102@gmail.com>
2023-11-12 00:44:16 +0900

    feat: 메뉴 이름, 개수 검증 기능 구현

--- src/main/java/christmas/domain/OrderMenu.java
+++ src/main/java/christmas/domain/OrderMenu.java
@@ -1,12 +1,32 @@
 package christmas.domain;
 
+import christmas.domain.enums.Menu;
+
 public class OrderMenu {
     private final String menu;
     private final int quantity;
 
     public OrderMenu(String menu, int quantity) {
+        validate(menu, quantity);
         this.menu = menu;
         this.quantity = quantity;
+    }
+
+    private void validate(String menu, int quantity) {
+        checkMenuName(menu);
+        checkQuantity(quantity);
+    }
+
+    private void checkMenuName(String menu) {
+        if (!Menu.isContainMenu(menu)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void checkQuantity(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 
     public String getMenu() {
