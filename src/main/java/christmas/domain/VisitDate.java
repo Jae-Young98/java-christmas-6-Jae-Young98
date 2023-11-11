422afda4d1711640b3eb8399e5e27462dc73e9dc
Jae-Young98 <ygy0102@gmail.com>
2023-11-12 01:20:31 +0900

    feat: 방문 날짜를 요일로 반환하는 기능 구현

--- src/main/java/christmas/domain/VisitDate.java
+++ src/main/java/christmas/domain/VisitDate.java
@@ -1,17 +1,27 @@
 package christmas.domain;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class VisitDate {
     private final int date;
 
     public VisitDate(int date) {
-        validateRange(date);
+        validateRange();
         this.date = date;
     }
 
-    private void validateRange(int date) {
+    private void validateRange() {
         if (date < 1 || date > 31) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
+    }
+
+    public String getDay() {
+        LocalDate localDate = LocalDate.of(2023, 12, this.date);
+        DayOfWeek day = localDate.getDayOfWeek();
+
+        return day.toString();
     }
 
     public int getDate() {
