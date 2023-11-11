1eecbaa7f6e740f1b05a10e5d29cd6a1e8021263
Jae-Young98 <ygy0102@gmail.com>
2023-11-12 02:46:40 +0900

    test: 주문 예외 발생 테스트

---
+++ src/test/java/christmas/domain/OrderTest.java
@@ -0,0 +1,61 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class OrderTest {
+    private Order order;
+
+    @BeforeEach
+    void init() {
+        order = new Order();
+    }
+
+    @DisplayName("올바른 주문시 예외가 발생하지 않는다.")
+    @Test
+    void validate() {
+        // given
+        OrderMenu menu1 = new OrderMenu("해산물파스타", 2);
+        OrderMenu menu2 = new OrderMenu("샴페인", 1);
+        order.addMenu(menu1);
+        order.addMenu(menu2);
+
+        // when, then
+        assertThatNoException().isThrownBy(() -> order.check());
+    }
+
+    @DisplayName("음료만 주문시 예외를 발생 시킨다.")
+    @Test
+    void checkOnlyDrink() {
+        // given
+        OrderMenu menu1 = new OrderMenu("제로콜라", 2);
+        OrderMenu menu2 = new OrderMenu("샴페인", 1);
+        order.addMenu(menu1);
+        order.addMenu(menu2);
+
+        // when, then
+        assertThatThrownBy(() -> order.check())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("음료만 주문");
+    }
+
+    @DisplayName("최대 주문 개수를 초과할 경우 예외를 발생 시킨다.")
+    @Test
+    void checkQuantities() {
+        // given
+        OrderMenu menu1 = new OrderMenu("티본스테이크", 15);
+        OrderMenu menu2 = new OrderMenu("바비큐립", 6);
+        order.addMenu(menu1);
+        order.addMenu(menu2);
+
+        // when, then
+        assertThatThrownBy(() -> order.check())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.");
+    }
+}
