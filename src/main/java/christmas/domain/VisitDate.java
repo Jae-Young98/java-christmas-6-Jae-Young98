0b263eb40b0e0f6ceb62e45152c4d170752a7fd8
Jae-Young98 <ygy0102@gmail.com>
2023-11-12 01:21:50 +0900

    refactor: 방문 날짜 검증 메소드 매개변수 추가

--- src/main/java/christmas/domain/VisitDate.java
+++ src/main/java/christmas/domain/VisitDate.java
@@ -7,11 +7,11 @@ public class VisitDate {
     private final int date;
 
     public VisitDate(int date) {
-        validateRange();
+        validateRange(date);
         this.date = date;
     }
 
-    private void validateRange() {
+    private void validateRange(int date) {
         if (date < 1 || date > 31) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
