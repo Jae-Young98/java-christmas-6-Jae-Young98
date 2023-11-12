ee990f550c085d89e93459f86f64a11508409110
Jae-Young98 <ygy0102@gmail.com>
2023-11-13 04:07:40 +0900

    feat: 메뉴 입력시 구분자(,)와 공백을 검증하는 기능 구현

--- src/main/java/christmas/utils/InputValidator.java
+++ src/main/java/christmas/utils/InputValidator.java
@@ -1,15 +1,35 @@
 package christmas.utils;
 
+import java.util.List;
+
 public class InputValidator {
+    private static final String DELIMITER_COMMA = ",";
     private static final String DIGIT_REGEX = "^[0-9]+$";
 
-    public static void validateNumber(String number) {
+    public static void validateVisitDate(String number) {
         if (isNotDigit(number)) {
-            throw new IllegalArgumentException("숫자만 입력 가능합니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다. 다시 입력해 주세요.");
         }
     }
 
     private static boolean isNotDigit(String number) {
         return !number.matches(DIGIT_REGEX);
+    }
+
+    public static void validateMenus(String menus) {
+        isEmpty(menus);
+        isEndWithDelimiter(menus);
+    }
+
+    public static void isEmpty(String menus) {
+        if (menus.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 공백은 입력될 수 없습니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public static void isEndWithDelimiter(String menus) {
+        if (menus.endsWith(DELIMITER_COMMA)) {
+            throw new IllegalArgumentException("[ERROR] 입력은 구분자(,)로 끝날 수 없습니다. 다시 입력해 주세요.");
+        }
     }
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -22,7 +22,7 @@ public class InputView {
     public String inputMenus() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
-
+        InputValidator.validateMenus(input);
         return input;
     }
 }
