4c4f9a365221e9ed0e5d338ceef0824da24d33fe
Jae-Young98 <ygy0102@gmail.com>
2023-11-13 04:10:18 +0900

    feat: 메뉴 입력을 Map으로 변환 시키는 기능 구현
    - Map으로 변환 시키며 검증을 거치도록 구현
    - 검증목록: 구분자(-), 한글 여부, 중복

---
+++ src/main/java/christmas/utils/Converter.java
@@ -0,0 +1,84 @@
+package christmas.utils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Converter {
+    private static final String STRING_REGEX = "^[ㄱ-ㅎ가-힣]*$";
+    private static final String DIGIT_REGEX = "^[0-9]+$";
+    private static final String DELIMITER_COMMA = ",";
+    private static final String DELIMITER_DASH = "-";
+
+    public static List<String> convertList(String menus) {
+        return Arrays.asList(menus.split(DELIMITER_COMMA));
+    }
+
+    public static Map<String, Integer> convertMap(List<String> menus) {
+        return makeMap(menus);
+    }
+
+    private static Map<String, Integer> makeMap(List<String> menus) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String str : menus) {
+            String[] splitMenu = str.split(DELIMITER_DASH);
+            validateSplitMenu(splitMenu);
+            String menu = splitMenu[0].trim();
+            int count = Integer.parseInt(splitMenu[1].trim());
+            validateDuplicate(map, menu);
+            map.put(menu, count);
+        }
+
+        return map;
+    }
+
+    private static void validateSplitMenu(String[] splitMenu) {
+        validateLength(splitMenu);
+        validateMenu(splitMenu);
+        validateCount(splitMenu);
+    }
+
+    private static void validateLength(String[] splitMenu) {
+        if (wrongLength(splitMenu)) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static boolean wrongLength(String[] splitMenu) {
+        return splitMenu.length != 2;
+    }
+
+    private static void validateMenu(String[] splitMenu) {
+        String menu = splitMenu[0].trim();
+        if (isNotString(menu)) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 메뉴 입력입니다. 한글로 다시 입력해 주세요.");
+        }
+    }
+
+    private static void validateCount(String[] splitMenu) {
+        String count = splitMenu[1].trim();
+        if (isNotDigit(count)) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 개수 입력입니다. 숫자로 다시 입력해 주세요.");
+        }
+    }
+
+    private static boolean isNotString(String menu) {
+        return !menu.matches(STRING_REGEX);
+    }
+
+    private static boolean isNotDigit(String number) {
+        return !number.matches(DIGIT_REGEX);
+    }
+
+    private static void validateDuplicate(Map<String, Integer> map, String name) {
+        if (hasDuplicate(map, name)) {
+            throw new IllegalArgumentException("[ERROR] 메뉴 이름은 중복 될 수 없습니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static boolean hasDuplicate(Map<String, Integer> map, String name) {
+        return map.containsKey(name);
+    }
+}
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -19,10 +19,11 @@ public class InputView {
         return Integer.parseInt(input);
     }
 
-    public String inputMenus() {
+    public Map<String, Integer> inputMenus() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
         InputValidator.validateMenus(input);
-        return input;
+        List<String> menus = Converter.convertList(input);
+        return Converter.convertMap(menus);
     }
 }
