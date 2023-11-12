a9893a2162d206c35f525fc9fd78637577e60178
Jae-Young98 <ygy0102@gmail.com>
2023-11-13 04:07:14 +0900

    feat: 메뉴 입력을 받는 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,7 +1,10 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.utils.Converter;
 import christmas.utils.InputValidator;
+import java.util.List;
+import java.util.Map;
 
 public class InputView {
     public void showGreeting() {
@@ -11,8 +14,15 @@ public class InputView {
     public int inputVisitDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
-        InputValidator.validateNumber(input);
+        InputValidator.validateVisitDate(input);
 
         return Integer.parseInt(input);
+    }
+
+    public String inputMenus() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = Console.readLine();
+
+        return input;
     }
 }
