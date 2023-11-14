6b89303f54c0ce657e18253938b410c15e18a0c7
Jae-Young98 <ygy0102@gmail.com>
2023-11-15 01:11:39 +0900

    fix: 배지 증정이 안되던 오류 수정
    - 배지의 minDiscountAmount를 음수로 수정

--- src/main/java/christmas/domain/enums/Badge.java
+++ src/main/java/christmas/domain/enums/Badge.java
@@ -3,22 +3,22 @@ package christmas.domain.enums;
 import java.util.Arrays;
 
 public enum Badge {
-    SANTA("산타", 20000),
-    TREE("트리", 10000),
-    START("별", 5000),
+    SANTA("산타", -20000),
+    TREE("트리", -10000),
+    START("별", -5000),
     NONE("없음", 0);
 
     private final String name;
     private final int minDiscountAmount;
 
-    Badge(String name, int discountAmount) {
+    Badge(String name, int minDiscountAmount) {
         this.name = name;
-        this.minDiscountAmount = discountAmount;
+        this.minDiscountAmount = minDiscountAmount;
     }
 
     public static Badge getBadge(final int allDiscountAmount) {
         return Arrays.stream(Badge.values())
-                .filter(badge -> allDiscountAmount >= badge.minDiscountAmount)
+                .filter(badge -> allDiscountAmount <= badge.minDiscountAmount)
                 .findFirst()
                 .orElse(NONE);
     }
